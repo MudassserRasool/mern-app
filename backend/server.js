1b6f1c971d0ed3f6010eeb6c07b5874e11fbf144
mudasserasool <mudasserasool@gmail.com>
2023-10-08 18:36:16 +0500

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,20 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 // deployment
-app.use(
-  cors({
-    origin: [''],
-    methods: ['GET', 'POST'],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [''],
+//     methods: ['GET', 'POST'],
+//     credentials: true,
+//   })
+// );
 
 // middleware
 app.use(express.json());
 
-mongoose.connect(
-  'mongodb://mern2022:mern2022@ac-1vocr4t-shard-00-00.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-01.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-02.43aaypx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x4e2nh-shard-0&authSource=admin&retryWrites=true&w=majority'
-);
+// mongoose.connect(
+//   'mongodb://mern2022:mern2022@ac-1vocr4t-shard-00-00.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-01.43aaypx.mongodb.net:27017,ac-1vocr4t-shard-00-02.43aaypx.mongodb.net:27017/?ssl=true&replicaSet=atlas-x4e2nh-shard-0&authSource=admin&retryWrites=true&w=majority'
+// );
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
